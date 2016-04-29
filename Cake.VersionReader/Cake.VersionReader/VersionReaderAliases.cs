08c6f12a1579ad83bee266b822af057beca1d6e0
Ralph Viktor <ralph@parcelninja.com>
2016-04-29 16:47:17 +0200

    Initial code commit.

---
+++ Cake.VersionReader/Cake.VersionReader/VersionReaderAliases.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.Core.IO;
+
+namespace Cake.VersionReader
+{
+    /// <summary>
+    /// Version Reader Aliases
+    /// </summary>
+    [CakeAliasCategory("Version Reader")]
+    public static class VersionReaderAliases
+    {
+        /// <summary>
+        /// Get the version number from an assembly.
+        /// </summary>
+        /// <param name="context">The context</param>
+        /// <param name="file">The binary to read from</param>
+        /// <returns>Version number in the format '0.0.0.0'</returns>
+        [CakeMethodAlias]
+        public static string GetVersionNumber(this ICakeContext context, FilePath file)
+        {
+            var filePath = file.MakeAbsolute(context.Environment).FullPath;
+            var version = AssemblyName.GetAssemblyName(filePath).Version;
+
+            return version.ToString();
+        }
+
+        /// <summary>
+        /// Get the version number with the current build number appended.
+        /// This is based on the article found here: http://www.xavierdecoster.com/semantic-versioning-auto-incremented-nuget-package-versions
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="file"></param>
+        /// <param name="buildNumber"></param>
+        /// <returns>Version number in the format '0.0.0.0-CI00000'</returns>
+        public static string GetVersionNumberWithContinuesIntegrationNumberAppended(this ICakeContext context, FilePath file, int buildNumber)
+        {
+            var filePath = file.MakeAbsolute(context.Environment).FullPath;
+            var version = AssemblyName.GetAssemblyName(filePath).Version.ToString();
+            var adjustedVersion = $"{version}-CI{buildNumber.ToString("00000")}";
+            return adjustedVersion;
+        }
+    }
+}
