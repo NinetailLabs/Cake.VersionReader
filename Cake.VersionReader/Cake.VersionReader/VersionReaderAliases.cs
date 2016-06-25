b29ae0dd53e119ede48065431c276209435f02c2
Ralph Viktor <ralphviktor@gmail.com>
2016-06-25 08:30:16 +0200

    feat: Added methods to create 4-digit version numbers #VREAD-1 Fixed

--- Cake.VersionReader/Cake.VersionReader/VersionReaderAliases.cs
+++ Cake.VersionReader/Cake.VersionReader/VersionReaderAliases.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using Cake.Core;
 using Cake.Core.Annotations;
 using Cake.Core.IO;
@@ -12,18 +13,29 @@ namespace Cake.VersionReader
     public static class VersionReaderAliases
     {
         /// <summary>
-        /// Get the version number from an assembly.
+        /// Get the version number from an assembly in SemVer format.
+        /// </summary>
+        /// <param name="context">The context</param>
+        /// <param name="file">The binary to read from</param>
+        /// <returns>Version number in the format '0.0.0'</returns>
+        [CakeMethodAlias]
+        public static string GetVersionNumber(this ICakeContext context, FilePath file)
+        {
+            var version = GetVersionFromFile(context, file);
+            return $"{version.Major}.{version.Minor}.{version.Build}";
+        }
+
+        /// <summary>
+        /// Get the version number from an assembly in 4-digit format.
         /// </summary>
         /// <param name="context">The context</param>
         /// <param name="file">The binary to read from</param>
         /// <returns>Version number in the format '0.0.0.0'</returns>
         [CakeMethodAlias]
-        public static string GetVersionNumber(this ICakeContext context, FilePath file)
+        public static string GetFullVersionNumber(this ICakeContext context, FilePath file)
         {
-            var filePath = file.MakeAbsolute(context.Environment).FullPath;
-            var version = AssemblyName.GetAssemblyName(filePath).Version;
-
-            return $"{version.Major}.{version.Minor}.{version.Build}";
+            var version = GetVersionFromFile(context, file);
+            return $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
         }
 
         /// <summary>
@@ -32,15 +44,42 @@ namespace Cake.VersionReader
         /// </summary>
         /// <param name="context">The context</param>
         /// <param name="file">The binary to read from</param>
-        /// <param name="buildNumber">The build number as provided by the build serveer</param>
-        /// <returns>Version number in the format '0.0.0.0-CI00000'</returns>
+        /// <param name="buildNumber">The build number as provided by the build server</param>
+        /// <returns>Version number in the format '0.0.0-CI00000'</returns>
         [CakeMethodAlias]
         public static string GetVersionNumberWithContinuesIntegrationNumberAppended(this ICakeContext context, FilePath file, int buildNumber)
         {
-            var filePath = file.MakeAbsolute(context.Environment).FullPath;
-            var version = AssemblyName.GetAssemblyName(filePath).Version;
+            var version = GetVersionFromFile(context, file);
             var adjustedVersion = $"{version.Major}.{version.Minor}.{version.Build}-CI{buildNumber.ToString("00000")}";
             return adjustedVersion;
+        }
+
+        /// <summary>
+        /// Get the version number from an assembly in 4-digit format with the current build number appended.
+        /// </summary>
+        /// <param name="context">The context</param>
+        /// <param name="file">The binary to read from</param>
+        /// <param name="buildNumber">The build number as provided by the build server</param>
+        /// <returns>Version number in the format '0.0.0.0-CI00000</returns>
+        [CakeMethodAlias]
+        public static string GetFullVersionNumberWithContinuesIntegrationNumberAppended(this ICakeContext context, FilePath file, int buildNumber)
+        {
+            var version = GetVersionFromFile(context, file);
+            var adjustedVersion = $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}-CI{buildNumber.ToString("00000")}";
+            return adjustedVersion;
+        }
+
+        /// <summary>
+        /// Retrieve version information from a binary
+        /// </summary>
+        /// <param name="context">The context</param>
+        /// <param name="file">The binary to read from</param>
+        /// <returns>Version</returns>
+        private static Version GetVersionFromFile(ICakeContext context, FilePath file)
+        {
+            var filePath = file.MakeAbsolute(context.Environment).FullPath;
+            var version = AssemblyName.GetAssemblyName(filePath).Version;
+            return version;
         }
     }
 }
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 ##Cake.VersionReader
 
-An addin for [CakeBuild](http://cakebuild.net/) that allows for the easy retrieval of version numbers from an assembly. Also support creating CI version number based that fit with SemVer based on [this](http://www.xavierdecoster.com/semantic-versioning-auto-incremented-nuget-package-versions) article.
+An addin for [CakeBuild](http://cakebuild.net/) that allows for the easy retrieval of version numbers from an assembly. Also support creating CI version numbers that fit with SemVer based on [this](http://www.xavierdecoster.com/semantic-versioning-auto-incremented-nuget-package-versions) article.
 
 You can easilty reference Cake.VersionReader directly in you build script using Cake's Addin syntax:
 ```csharp
@@ -9,11 +9,22 @@ You can easilty reference Cake.VersionReader directly in you build script using 
 
 ###Methods
 The following methods are provided:
-*GetVersionNumber(this ICakeContext context, FilePath file)*
-- Get the version number for the spesified file in the format "0.0.0"
 
-*GetVersionNumberWithContinuesIntegrationNumberAppended(this ICakeContext context, FilePath file, int buildNumber)*
-- Get the CI version number in the format "0.0.0-CI00000"
+- GetVersionNumber(this ICakeContext context, FilePath file)
+
+*Get the version number for the spesified file in the SemVer format "0.0.0"*
+
+- GetFullVersionNumber(this ICakeContext context, FilePath file)
+
+*Get the version number for the spesified file in 4-digit format "0.0.0.0"*
+
+- GetVersionNumberWithContinuesIntegrationNumberAppended(this ICakeContext context, FilePath file, int buildNumber)
+
+*Get the CI version number in the format "0.0.0-CI00000"*
+
+- GetFullVersionNumberWithContinuesIntegrationNumberAppended(this ICakeContext context, FilePath file, int buildNumber)
+
+*Get the CI version number in the format "0.0.0.0-CI00000"*
 
 ###Icon
 [Cyclops](https://thenounproject.com/term/cyclops/60203/) by [Mike Hince](https://thenounproject.com/zer0mike/) from the Noun Project, remixed with the Cake icon.
